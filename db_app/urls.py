acf76f7cc998858fbf1cc91f89c363f9ed73715a
Fatih Demir <101219059+fatih260@users.noreply.github.com>
2024-05-04 22:14:12 +0300

    database connection try

---
+++ db_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    #path('login/', views.log_in, name='login'),
+    #path('csrf_token/', views.csrf_token, name='csrf_token'),
+    #path('session/', views.session, name='session'),
+]
--- db_project/settings.py
+++ db_project/settings.py
@@ -103,6 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'db_app.User'
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
